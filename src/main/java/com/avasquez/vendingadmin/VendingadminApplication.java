1568b9f524f5ddf4f467fcf308af7210b9168ae3
jaguarsoftsv <jaguarsoftsv@gmail.com>
2020-06-25 22:51:12 -0600

    Initial Commit. Adding sources from spring initializr

---
+++ src/main/java/com/avasquez/vendingadmin/VendingadminApplication.java
@@ -0,0 +1,13 @@
+package com.avasquez.vendingadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendingadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendingadminApplication.class, args);
+	}
+
+}
