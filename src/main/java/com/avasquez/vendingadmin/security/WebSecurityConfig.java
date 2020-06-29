b834a1585bc9b8f349ae2920179af17468c38df5
jaguarsoftsv <jaguarsoftsv@gmail.com>
2020-06-29 09:17:40 -0600

    Adding prod database config and dev persistent database

--- src/main/java/com/avasquez/vendingadmin/security/WebSecurityConfig.java
+++ src/main/java/com/avasquez/vendingadmin/security/WebSecurityConfig.java
@@ -68,9 +68,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().
                 authorizeRequests()
-                //.antMatchers("/api/auth/**", "**").permitAll()
-                //.antMatchers("/api/**").authenticated()
-                .anyRequest().permitAll()
+                .antMatchers("/api/auth/**", "**").permitAll()
+                .antMatchers("/api/**").authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
